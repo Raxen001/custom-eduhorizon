0b3f84296d4f226528bc623574cbc5ee9562ef75
raxen001 <raxen001@tutamail.com>
2024-12-12 05:20:48 +0530

    aasdfasdfadsfasfdadsf

--- build.sh
+++ build.sh
@@ -13,12 +13,12 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 
-bash -c 'echo "X-GNOME-Autostart-enabled=false" >> /etc/xdg/autostart/gnome-initial-setup-first-login.desktop'
+# bash -c 'echo "X-GNOME-Autostart-enabled=false" >> /etc/xdg/autostart/gnome-initial-setup-first-login.desktop'
 
-useradd student -p please@1234
-echo please@1234 | passwd student --stdin
-echo please@1234 | passwd root --stdin
-
+# useradd student -p please@1234
+# echo please@1234 | passwd student --stdin
+# echo please@1234 | passwd root --stdin
+#
 # cat <<EOF > /home/student/.local/bin/gnome-kiosk-script
 # #!/bin/sh
 #
@@ -32,31 +32,31 @@ echo please@1234 | passwd root --stdin
 # EOF
 
 
-su -l student -c "mkdir -p /var/home/student/"
-su -l student -c "mkdir -p /var/home/student/Desktop"
-su -l student -c "mkdir -p /var/home/student/Downloads"
-su -l student -c "mkdir -p /var/home/student/.config"
-su -l student -c "mkdir -p /var/home/student/.config/autostart"
-cat <<EOF > /var/home/student/.config/autostart/firefox.desktop
-[Desktop Entry]
-Version=1.0
-Name=Firefox
-GenericName=Web Browser
-Comment=Browse the Web
-# Exec=firefox %u
-Exec=firefox https://legion.tailaadcc.ts.net/
-Icon=firefox
-Terminal=false
-Type=Application
-MimeType=text/html;text/xml;application/xhtml+xml;application/vnd.mozilla.xul+xml;text/mml;x-scheme-handler/http;x-scheme-handler/https;
-StartupNotify=true
-Categories=Network;WebBrowser;
-Keywords=web;browser;internet;
-X-Desktop-File-Install-Version=0.27
-EOF
-
+# su -l student -c "mkdir -p /var/home/student/"
+# su -l student -c "mkdir -p /var/home/student/Desktop"
+# su -l student -c "mkdir -p /var/home/student/Downloads"
+# su -l student -c "mkdir -p /var/home/student/.config"
+# su -l student -c "mkdir -p /var/home/student/.config/autostart"
+# cat <<EOF > /var/home/student/.config/autostart/firefox.desktop
+# [Desktop Entry]
+# Version=1.0
+# Name=Firefox
+# GenericName=Web Browser
+# Comment=Browse the Web
+# # Exec=firefox %u
+# Exec=firefox https://legion.tailaadcc.ts.net/
+# Icon=firefox
+# Terminal=false
+# Type=Application
+# MimeType=text/html;text/xml;application/xhtml+xml;application/vnd.mozilla.xul+xml;text/mml;x-scheme-handler/http;x-scheme-handler/https;
+# StartupNotify=true
+# Categories=Network;WebBrowser;
+# Keywords=web;browser;internet;
+# X-Desktop-File-Install-Version=0.27
+# EOF
+#
 # this installs a package from fedora repos
-rpm-ostree install firefox
+rpm-ostree install firefox chromium
 # this would install a package from rpmfusion
 # rpm-ostree install codium
 # install gnome kiosk
--- gen_iso.sh
+++ gen_iso.sh
@@ -1,3 +1,12 @@
-docker run --rm --privileged --volume .:/build-container-installer/build  ghcr.io/jasonn3/build-container-installer:latest VERSION=40 IMAGE_REPO="ghcr.io/raxen001" IMAGE_NAME="custom-eduhorizon" IMAGE_TAG="latest" VARIANT=Silverblue
+docker run --rm --privileged \
+	--volume .:/build-container-installer/build  \
+	--volume ./installer/lorax_templates:/additional_lorax_templates \
+	ghcr.io/jasonn3/build-container-installer:latest \
+	ADDITIONAL_TEMPLATES="/additional_lorax_templates/remove_root_password_prompt.tmpl" \
+	VERSION=40 \
+	IMAGE_REPO="ghcr.io/raxen001" \
+	IMAGE_NAME="custom-eduhorizon"\
+	IMAGE_TAG="latest"\
+	VARIANT=Silverblue
 sudo chown raxen:raxen deploy.iso
 chmod +x deploy.iso
